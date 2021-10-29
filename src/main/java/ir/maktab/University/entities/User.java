a72d4b08c18406b76945969fd8599d7ccd2812ec
Matin <matin.parsapour.iam@gmail.com>
2021-10-29 21:45:01 +0330

    Create User super class

---
+++ src/main/java/ir/maktab/University/entities/User.java
@@ -0,0 +1,52 @@
+package ir.maktab.University.entities;
+
+import ir.maktab.University.entities.base.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@Inheritance
+@Table(name = User.TABLE_NAME)
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User extends BaseEntity<Long> {
+    public static final String TABLE_NAME = "user_table";
+    private static final String FIRST_NAME = "first_name";
+    private static final String LAST_NAME = "last_name";
+    private static final String BIRTHDAY = "birthday";
+    private static final String EMAIL = "email";
+    private static final String TYPE = "type";
+    private static final String USER_NAME = "user_name";
+    private static final String PASSWORD = "password";
+
+    @Column(name = FIRST_NAME)
+    private String firstName;
+
+    @Column(name = LAST_NAME)
+    private String lastName;
+
+    @Column(name = BIRTHDAY)
+    private LocalDate birthday;
+
+    @Column(name = EMAIL)
+    private String email;
+
+    @Column(name = TYPE)
+    private String type;
+
+    @Column(name = USER_NAME,unique = true)
+    private String userName;
+
+    @Column(name = PASSWORD)
+    private String password;
+}
