edce470493ee1e8be5fb73daf4327f2d97ef4c0a
Matin <matin.parsapour.iam@gmail.com>
2021-10-29 21:45:24 +0330

    Create User children

---
+++ src/main/java/ir/maktab/University/entities/Manager.java
@@ -0,0 +1,10 @@
+package ir.maktab.University.entities;
+import javax.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+public class Manager extends User{
+}
---
+++ src/main/java/ir/maktab/University/entities/Teacher.java
@@ -0,0 +1,22 @@
+package ir.maktab.University.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Teacher extends User{
+    private static final String STATUS = "status";
+
+    @Column(name = STATUS)
+    private String status;
+}
