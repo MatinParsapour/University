792e45ceeb49232aa34d74c9ca5034b02a41d54b
Matin <matin.parsapour.iam@gmail.com>
2021-10-29 21:44:36 +0330

    Create BaseEntity

---
+++ src/main/java/ir/maktab/University/entities/base/BaseEntity.java
@@ -0,0 +1,20 @@
+package ir.maktab.University.entities.base;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@MappedSuperclass
+@Setter
+@Getter
+public class BaseEntity<ID extends Serializable> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private ID id;
+}
