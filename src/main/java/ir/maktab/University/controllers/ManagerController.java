e6abb5656ed68cc0ea4abf52ce8b1c55c90091b3
Matin <matin.parsapour.iam@gmail.com>
2021-10-31 12:18:06 +0330

    Update ManagerController to get all students and teachers

--- src/main/java/ir/maktab/University/controllers/ManagerController.java
+++ src/main/java/ir/maktab/University/controllers/ManagerController.java
@@ -1,12 +1,33 @@
 package ir.maktab.University.controllers;
 
+import ir.maktab.University.entities.Student;
+import ir.maktab.University.entities.Teacher;
 import ir.maktab.University.service.ManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class ManagerController {
 
     @Autowired
     private ManagerService managerService;
+
+    @Autowired
+    private TeacherController teacherController;
+
+    @Autowired
+    private StudentController studentController;
+
+    @GetMapping("/manager-main")
+    public String managerMain(Model model){
+        List<Student> studentList = studentController.students();
+        List<Teacher> teacherList = teacherController.teachers();
+        model.addAttribute("students", studentList);
+        model.addAttribute("teachers",teacherList);
+        return "Manager";
+    }
 }
