dc632e1ac0a4fdab9340ce5fb4a378fcdd10a5e1
Matin <matin.parsapour.iam@gmail.com>
2021-10-31 12:19:57 +0330

    Update StudentController to reject, accept, delete or change role to student for user

--- src/main/java/ir/maktab/University/controllers/StudentController.java
+++ src/main/java/ir/maktab/University/controllers/StudentController.java
@@ -5,7 +5,10 @@ import ir.maktab.University.entities.User;
 import ir.maktab.University.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class StudentController {
@@ -14,7 +17,7 @@ public class StudentController {
     private StudentService studentService;
 
 
-    public void addStudent(User user){
+    public Student addStudent(User user){
         Student student = new Student();
         student.setStatus("In progerss");
         student.setFirstName(user.getFirstName());
@@ -25,6 +28,38 @@ public class StudentController {
         student.setNationalCode(user.getNationalCode());
         student.setEmail(user.getEmail());
         student.setType(user.getType());
+        studentService.addStudent(student);
+        return student;
+    }
+
+    @GetMapping("/get-students")
+    public List<Student> students(){
+        return studentService.getUnConfirmedStudents();
+    }
+
+    @PostMapping("/reject-student")
+    public void rejectStudent(String userId){
+        Student student = studentService.getStudentById(Long.parseLong(userId)).get();
+        student.setStatus("Rejected");
+        studentService.addStudent(student);
+    }
+
+    @PostMapping("/accept-student")
+    public void acceptStudent(String userId){
+        Student student = studentService.getStudentById(Long.parseLong(userId)).get();
+        student.setStatus("Accepted");
+        studentService.addStudent(student);
+    }
+
+    @PostMapping("/delete-student")
+    public void deleteStudent(String userId){
+        studentService.deleteStudent(Long.parseLong(userId));
+    }
+
+    @PostMapping("/change-to-student")
+    public void changeToStudent(User user) {
+        Student student = addStudent(user);
+        student.setStatus("Accepted");
         studentService.addStudent(student);
     }
 }
