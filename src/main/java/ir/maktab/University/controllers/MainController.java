c6d6a57ed0dcec94c2a559661c031140bd3aabe1
Matin <matin.parsapour.iam@gmail.com>
2021-10-31 16:37:46 +0330

    Update main controller to build a model

--- src/main/java/ir/maktab/University/controllers/MainController.java
+++ src/main/java/ir/maktab/University/controllers/MainController.java
@@ -1,13 +1,16 @@
 package ir.maktab.University.controllers;
 
+import ir.maktab.University.util.Security;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("user", Security.getUser());
         return "Home";
     }
 }
