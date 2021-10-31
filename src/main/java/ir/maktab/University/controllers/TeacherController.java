ad066b7e1523f630c1fd25141aa857ea0f54d74d
Matin <matin.parsapour.iam@gmail.com>
2021-10-31 12:21:59 +0330

    Update TeacherController to reject, accept delete or change user role to teacher

--- src/main/java/ir/maktab/University/controllers/TeacherController.java
+++ src/main/java/ir/maktab/University/controllers/TeacherController.java
@@ -5,16 +5,20 @@ import ir.maktab.University.entities.User;
 import ir.maktab.University.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class TeacherController {
 
     @Autowired
     private TeacherService teacherService;
 
 
-    public void addTeacher(User user){
+    public Teacher addTeacher(User user){
         Teacher teacher = new Teacher();
         teacher.setStatus("In progress");
         teacher.setFirstName(user.getFirstName());
@@ -26,5 +30,37 @@ public class TeacherController {
         teacher.setEmail(user.getEmail());
         teacher.setType(user.getType());
         teacherService.addTeacher(teacher);
+        return teacher;
+    }
+
+    @GetMapping("/get-teachers")
+    public List<Teacher> teachers(){
+        return teacherService.getUnConfirmedTeachers();
+    }
+
+    @PostMapping("/reject-teacher")
+    public void rejectTeacher(String userId){
+        Teacher teacher = teacherService.getTeacherById(Long.parseLong(userId)).get();
+        teacher.setStatus("Rejected");
+        teacherService.addTeacher(teacher);
+    }
+
+    @PostMapping("/accept-teacher")
+    public void acceptTeacher(String userId){
+        Teacher teacher = teacherService.getTeacherById(Long.parseLong(userId)).get();
+        teacher.setStatus("Accepted");
+        teacherService.addTeacher(teacher);
+    }
+
+    @PostMapping("/change-to-teacher")
+    public void changeToTeacher(User user){
+        Teacher teacher = addTeacher(user);
+        teacher.setStatus("Accepted");
+        teacherService.addTeacher(teacher);
+    }
+
+    @PostMapping("/delete-teacher")
+    public void deleteTeacher(String userId){
+        teacherService.deleteTeacher(Long.parseLong(userId));
     }
 }
