23e25c22253abf8693eed7060d11ae41fe1ad4ba
Matin <matin.parsapour.iam@gmail.com>
2021-11-02 14:57:32 +0330

    Update CourseService and CourseServiceImpl to create get by id and get all courses

--- src/main/java/ir/maktab/University/service/CourseService.java
+++ src/main/java/ir/maktab/University/service/CourseService.java
@@ -1,4 +1,15 @@
 package ir.maktab.University.service;
 
+import ir.maktab.University.entities.Course;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CourseService {
+
+    void createCourse(Course course);
+
+    Optional<Course> getCourse(long id);
+
+    List<Course> getAllCourses();
 }
--- src/main/java/ir/maktab/University/service/impl/CourseServiceImpl.java
+++ src/main/java/ir/maktab/University/service/impl/CourseServiceImpl.java
@@ -1,13 +1,32 @@
 package ir.maktab.University.service.impl;
 
+import ir.maktab.University.entities.Course;
 import ir.maktab.University.repository.CourseRepository;
 import ir.maktab.University.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CourseServiceImpl implements CourseService {
 
     @Autowired
     private CourseRepository courseRepository;
+
+    @Override
+    public void createCourse(Course course) {
+        courseRepository.save(course);
+    }
+
+    @Override
+    public Optional<Course> getCourse(long id) {
+        return courseRepository.findById(id);
+    }
+
+    @Override
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
+    }
 }
