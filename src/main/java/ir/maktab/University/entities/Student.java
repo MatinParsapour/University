e6cab800a51d592865537c16b378f5372d677d8e
Matin <matin.parsapour.iam@gmail.com>
2021-11-04 20:31:31 +0330

    Update relationship between student and course

--- src/main/java/ir/maktab/University/entities/Student.java
+++ src/main/java/ir/maktab/University/entities/Student.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
@@ -22,6 +23,6 @@ public class Student extends User {
     @Column(name = STATUS)
     private String status;
 
-    @ManyToMany(mappedBy = "students")
+    @ManyToMany(mappedBy = "students", cascade = CascadeType.ALL)
     private List<Course> courseList;
 }
