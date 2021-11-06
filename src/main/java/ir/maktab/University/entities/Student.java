49bfbdbc909f835e952de2b06f6f8d844b18f4c0
Matin <matin.parsapour.iam@gmail.com>
2021-11-06 14:16:15 +0330

    Update relationship between student and course

--- src/main/java/ir/maktab/University/entities/Student.java
+++ src/main/java/ir/maktab/University/entities/Student.java
@@ -10,7 +10,8 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Setter
@@ -25,8 +26,8 @@ public class Student extends User {
     @Column(name = STATUS)
     private String status;
 
-    @ManyToMany(cascade = CascadeType.ALL)
-    private List<Course> courseList;
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "students")
+    private Set<Course> courseList = new HashSet<>();
 
     @Column(name = IS_ACTIVE)
     private boolean isActive;
