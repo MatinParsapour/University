95f04b2c0c4346ebe0fe8a2c577e23dc1260ac7d
Matin <matin.parsapour.iam@gmail.com>
2021-11-06 15:01:29 +0330

    Check dates before submit form

--- src/main/resources/static/js/CourseJS.js
+++ src/main/resources/static/js/CourseJS.js
@@ -14,3 +14,20 @@
             }, false)
         })
 })()
+
+
+document.getElementById("createCourseButton").addEventListener("click",function (event){
+    let valueStartDate = document.getElementById("validationCustomUsername").value;
+    let valueFinishDate = document.getElementById("validationCustom03").value;
+    var startDate = new Date(valueStartDate);
+    var finishDate = new Date(valueFinishDate);
+    if(startDate.getDate() < new Date().getDate()){
+        event.preventDefault();
+        alert("تاریخ انتخابی اشتباه است " +
+            "باید بزرگتر از تاریخ فعلی باشد")
+    }
+    if(finishDate.getDate() < startDate.getDate()){
+        event.preventDefault();
+        alert("تاریخ پایانی دوره باید بعد از تاریخ شروع دوره باشد")
+    }
+})
