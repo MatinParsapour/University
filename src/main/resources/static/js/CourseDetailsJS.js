d5eaa8cd0bf8d8dde745cb5df648995df60198f4
Matin <matin.parsapour.iam@gmail.com>
2021-11-08 18:13:57 +0330

    Update CourseDetails.html to create new quiz and delete a quiz and create a js file and update css file

---
+++ src/main/resources/static/js/CourseDetailsJS.js
@@ -0,0 +1,9 @@
+function openCreateQuizPad() {
+    document.getElementById("coursesPad").style.display = "block"
+    document.getElementById("closeButton").style.display = "block"
+}
+
+function closeCreateQuizPad() {
+    document.getElementById("coursesPad").style.display = "none"
+    document.getElementById("closeButton").style.display = "none"
+}
