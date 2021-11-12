58874dc4450708f1028ed27e074f2db8ebd9ce31
Matin <matin.parsapour.iam@gmail.com>
2021-11-12 21:32:44 +0330

    Create controller for question and questions

---
+++ src/main/java/ir/maktab/University/controllers/QuestionController.java
@@ -0,0 +1,18 @@
+package ir.maktab.University.controllers;
+
+import ir.maktab.University.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/question")
+public class QuestionController {
+
+    private final QuestionService questionService;
+
+    @Autowired
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+}
---
+++ src/main/java/ir/maktab/University/controllers/QuestionsController.java
@@ -0,0 +1,18 @@
+package ir.maktab.University.controllers;
+
+import ir.maktab.University.service.QuestionsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/questions")
+public class QuestionsController {
+
+    private final QuestionsService questionsService;
+
+    @Autowired
+    public QuestionsController(QuestionsService questionsService) {
+        this.questionsService = questionsService;
+    }
+}
