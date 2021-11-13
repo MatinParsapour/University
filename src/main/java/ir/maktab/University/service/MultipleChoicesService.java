21f0379b1ec78d33952062fb125cfae9bf0db24b
Matin <matin.parsapour.iam@gmail.com>
2021-11-13 20:32:33 +0330

    Add a method to create a new multiple choices question

--- src/main/java/ir/maktab/University/service/MultipleChoicesService.java
+++ src/main/java/ir/maktab/University/service/MultipleChoicesService.java
@@ -4,4 +4,11 @@ import ir.maktab.University.entities.MultipleChoices;
 import ir.maktab.University.entities.QuestionAnswer;
 
 public interface MultipleChoicesService extends BaseService<MultipleChoices,Long> {
+
+    /**
+     * Object of multiple choices will save to database
+     * @param multipleChoices object of multiple choices includes : title, header(problem), options, correct answer
+     * @param grade point of the question
+     */
+    void createNewMultipleChoices(MultipleChoices multipleChoices, double grade, String option);
 }
--- src/main/java/ir/maktab/University/service/impl/MultipleChoicesServiceImpl.java
+++ src/main/java/ir/maktab/University/service/impl/MultipleChoicesServiceImpl.java
@@ -1,10 +1,15 @@
 package ir.maktab.University.service.impl;
 
 import ir.maktab.University.entities.MultipleChoices;
+import ir.maktab.University.entities.Options;
 import ir.maktab.University.repository.MultipleChoicesRepository;
 import ir.maktab.University.service.MultipleChoicesService;
+import ir.maktab.University.service.QuestionHeaderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class MultipleChoicesServiceImpl extends BaseServiceImpl<MultipleChoices,Long, MultipleChoicesRepository>
@@ -12,9 +17,24 @@ public class MultipleChoicesServiceImpl extends BaseServiceImpl<MultipleChoices,
 
     private final MultipleChoicesRepository multipleChoicesRepository;
 
+    private final QuestionHeaderService questionHeaderService;
+
     @Autowired
-    public MultipleChoicesServiceImpl(MultipleChoicesRepository multipleChoicesRepository) {
+    public MultipleChoicesServiceImpl(MultipleChoicesRepository multipleChoicesRepository, QuestionHeaderService questionHeaderService) {
         super(multipleChoicesRepository);
         this.multipleChoicesRepository = multipleChoicesRepository;
+        this.questionHeaderService = questionHeaderService;
+    }
+
+    @Override
+    public void createNewMultipleChoices(MultipleChoices multipleChoices, double grade, String option) {
+        List<String> options = Arrays.asList(option.split(","));
+        for(String questionOptions : options){
+            Options newOptions = new Options();
+            newOptions.setOptions(questionOptions);
+            multipleChoices.getOptions().add(newOptions);
+        }
+        MultipleChoices savedMultipleChoices = save(multipleChoices);
+        questionHeaderService.createNewQuestionHeaderByMultipleChoices(savedMultipleChoices,grade);
     }
 }
