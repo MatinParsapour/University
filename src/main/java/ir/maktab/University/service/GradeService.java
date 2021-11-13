9da38e76c722fd68a63f1ff6a46f5ab40edb543b
Matin <matin.parsapour.iam@gmail.com>
2021-11-13 15:42:02 +0330

    Add a method to create and save and return a new object of grade

--- src/main/java/ir/maktab/University/service/GradeService.java
+++ src/main/java/ir/maktab/University/service/GradeService.java
@@ -2,6 +2,15 @@ package ir.maktab.University.service;
 
 import ir.maktab.University.entities.Grade;
 import ir.maktab.University.entities.MultipleChoices;
+import ir.maktab.University.entities.Questions;
 
 public interface GradeService extends BaseService<Grade,Long> {
+
+    /**
+     * Create new object of grade with the point teacher set
+     * And save to data base
+     * @param point point of the question teacher created
+     * @return an object of grade that saved to data base
+     */
+    Grade createNewGrade(double point);
 }
--- src/main/java/ir/maktab/University/service/impl/GradeServiceImpl.java
+++ src/main/java/ir/maktab/University/service/impl/GradeServiceImpl.java
@@ -1,6 +1,7 @@
 package ir.maktab.University.service.impl;
 
 import ir.maktab.University.entities.Grade;
+import ir.maktab.University.entities.Questions;
 import ir.maktab.University.repository.GradeRepository;
 import ir.maktab.University.service.GradeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,5 +17,13 @@ public class GradeServiceImpl extends BaseServiceImpl<Grade,Long, GradeRepositor
     public GradeServiceImpl(GradeRepository gradeRepository) {
         super(gradeRepository);
         this.gradeRepository = gradeRepository;
+    }
+
+    @Override
+    public Grade createNewGrade(double point) {
+        Grade grade = new Grade();
+        grade.setQuestionPoint(point);
+        Grade savedGrade = save(grade);
+        return savedGrade;
     }
 }
