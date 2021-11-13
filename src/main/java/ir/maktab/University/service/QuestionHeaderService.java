f01f94759e85c6b7ec56bfee959bb6da8a994d31
Matin <matin.parsapour.iam@gmail.com>
2021-11-13 15:42:24 +0330

    Add a method to create and save a new object of QuestionHeader

--- src/main/java/ir/maktab/University/service/QuestionHeaderService.java
+++ src/main/java/ir/maktab/University/service/QuestionHeaderService.java
@@ -1,7 +1,15 @@
 package ir.maktab.University.service;
 
+import ir.maktab.University.entities.Descriptive;
 import ir.maktab.University.entities.Question;
 import ir.maktab.University.entities.QuestionHeader;
 
 public interface QuestionHeaderService extends BaseService<QuestionHeader,Long> {
+
+    /**
+     * Create new object of header for the descriptive question teacher created
+     * @param descriptive object of descriptive question that saved to data base
+     * @param grade the point of the question
+     */
+    void createNewQuestionHeaderByDescriptive(Descriptive descriptive, double grade);
 }
--- src/main/java/ir/maktab/University/service/impl/QuestionHeaderServiceImpl.java
+++ src/main/java/ir/maktab/University/service/impl/QuestionHeaderServiceImpl.java
@@ -1,10 +1,14 @@
 package ir.maktab.University.service.impl;
 
+import ir.maktab.University.entities.Descriptive;
 import ir.maktab.University.entities.QuestionHeader;
 import ir.maktab.University.repository.QuestionHeaderRepository;
 import ir.maktab.University.service.QuestionHeaderService;
+import ir.maktab.University.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class QuestionHeaderServiceImpl extends BaseServiceImpl<QuestionHeader,Long, QuestionHeaderRepository>
@@ -12,9 +16,20 @@ public class QuestionHeaderServiceImpl extends BaseServiceImpl<QuestionHeader,Lo
 
     private final QuestionHeaderRepository questionHeaderRepository;
 
+    private final QuestionService questionService;
+
     @Autowired
-    public QuestionHeaderServiceImpl(QuestionHeaderRepository questionHeaderRepository) {
+    public QuestionHeaderServiceImpl(QuestionHeaderRepository questionHeaderRepository, QuestionService questionService) {
         super(questionHeaderRepository);
         this.questionHeaderRepository = questionHeaderRepository;
+        this.questionService = questionService;
+    }
+
+    @Override
+    public void createNewQuestionHeaderByDescriptive(Descriptive descriptive,double grade) {
+        QuestionHeader questionHeader = new QuestionHeader();
+        questionHeader.setDescriptive(descriptive);
+        QuestionHeader savedQuestionHeader = save(questionHeader);
+        questionService.createNewQuestion(savedQuestionHeader,grade);
     }
 }
