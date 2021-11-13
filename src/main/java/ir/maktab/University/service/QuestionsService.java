8c55cf324488f6019a293d8a517cf5bc42dfcf63
Matin <matin.parsapour.iam@gmail.com>
2021-11-13 15:43:28 +0330

    Add two methods to check if a object of questions exists or not then create or add an object of question

--- src/main/java/ir/maktab/University/service/QuestionsService.java
+++ src/main/java/ir/maktab/University/service/QuestionsService.java
@@ -1,6 +1,20 @@
 package ir.maktab.University.service;
 
+import ir.maktab.University.entities.Question;
 import ir.maktab.University.entities.Questions;
 
 public interface QuestionsService extends BaseService<Questions,Long> {
+
+    /**
+     * Create new object of questions include the question and the grade teacher set
+     * @param question object of question saved to data base
+     * @param grade the point of question
+     */
+    void addToQuestions(Question question,double grade);
+
+    /**
+     * check for quiz if it has a questions object or not
+     * @return true if quiz doesn't have questions
+     */
+    boolean checkQuestions();
 }
--- src/main/java/ir/maktab/University/service/impl/QuestionsServiceImpl.java
+++ src/main/java/ir/maktab/University/service/impl/QuestionsServiceImpl.java
@@ -1,8 +1,14 @@
 package ir.maktab.University.service.impl;
 
+import ir.maktab.University.entities.Grade;
+import ir.maktab.University.entities.Question;
 import ir.maktab.University.entities.Questions;
 import ir.maktab.University.repository.QuestionsRepository;
+import ir.maktab.University.service.GradeService;
+import ir.maktab.University.service.QuestionService;
 import ir.maktab.University.service.QuestionsService;
+import ir.maktab.University.service.QuizService;
+import ir.maktab.University.util.Security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,9 +18,37 @@ public class QuestionsServiceImpl extends BaseServiceImpl<Questions,Long, Questi
 
     private final QuestionsRepository questionsRepository;
 
+    private final GradeService gradeService;
+
+    private final QuizService quizService;
+
     @Autowired
-    public QuestionsServiceImpl(QuestionsRepository questionsRepository) {
+    public QuestionsServiceImpl(QuestionsRepository questionsRepository, GradeService gradeService, QuizService quizService) {
         super(questionsRepository);
         this.questionsRepository = questionsRepository;
+        this.gradeService = gradeService;
+        this.quizService = quizService;
+    }
+
+    @Override
+    public void addToQuestions(Question question,double grade) {
+        Grade newGrade = gradeService.createNewGrade(grade);
+        if(checkQuestions()){
+            Questions questions = Security.getQuiz().getQuestions();
+            questions.getQuestionList().add(question);
+            questions.getGradeList().add(newGrade);
+            Questions savedQuestions = save(questions);
+        }else{
+            Questions questions = new Questions();
+            questions.getQuestionList().add(question);
+            questions.getGradeList().add(newGrade);
+            Questions savedQuestions = save(questions);
+            quizService.addQuestions(savedQuestions);
+        }
+    }
+
+    @Override
+    public boolean checkQuestions() {
+        return Security.getQuiz().getQuestions() != null;
     }
 }
