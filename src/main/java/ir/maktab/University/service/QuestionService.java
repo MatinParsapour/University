da759da0772b66ee268c1a0f6a3b1b00ecdcd5f9
Matin <matin.parsapour.iam@gmail.com>
2021-11-13 15:42:40 +0330

    Add a method to create and save a new object of Question

--- src/main/java/ir/maktab/University/service/QuestionService.java
+++ src/main/java/ir/maktab/University/service/QuestionService.java
@@ -1,7 +1,15 @@
 package ir.maktab.University.service;
 
 import ir.maktab.University.entities.Question;
+import ir.maktab.University.entities.QuestionHeader;
 import ir.maktab.University.entities.Questions;
 
 public interface QuestionService extends BaseService<Question,Long> {
+
+    /**
+     * Create new question with question header includes object with type of question teacher created
+     * @param questionHeader an object of question header that saved to data base
+     * @param grade the point of the question
+     */
+    void createNewQuestion(QuestionHeader questionHeader,double grade);
 }
--- src/main/java/ir/maktab/University/service/impl/QuestionServiceImpl.java
+++ src/main/java/ir/maktab/University/service/impl/QuestionServiceImpl.java
@@ -1,8 +1,10 @@
 package ir.maktab.University.service.impl;
 
 import ir.maktab.University.entities.Question;
+import ir.maktab.University.entities.QuestionHeader;
 import ir.maktab.University.repository.QuestionRepository;
 import ir.maktab.University.service.QuestionService;
+import ir.maktab.University.service.QuestionsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +15,20 @@ public class QuestionServiceImpl extends BaseServiceImpl<Question,Long, Question
 
     private final QuestionRepository questionRepository;
 
+    private final QuestionsService questionsService;
+
     @Autowired
-    public QuestionServiceImpl(QuestionRepository questionRepository) {
+    public QuestionServiceImpl(QuestionRepository questionRepository, QuestionsService questionsService) {
         super(questionRepository);
         this.questionRepository = questionRepository;
+        this.questionsService = questionsService;
+    }
+
+    @Override
+    public void createNewQuestion(QuestionHeader questionHeader,double grade) {
+        Question question = new Question();
+        question.setQuestionHeader(questionHeader);
+        Question savedQuestion = save(question);
+        questionsService.addToQuestions(savedQuestion,grade);
     }
 }
