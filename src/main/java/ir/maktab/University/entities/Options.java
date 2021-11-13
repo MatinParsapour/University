0580c41e8ba94d03acc773906358e59d44684f46
Matin <matin.parsapour.iam@gmail.com>
2021-11-13 20:33:08 +0330

    Add setter and getter and no args constructor and all args constructor for options

--- src/main/java/ir/maktab/University/entities/Options.java
+++ src/main/java/ir/maktab/University/entities/Options.java
@@ -1,8 +1,17 @@
 package ir.maktab.University.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Embeddable;
 
 @Embeddable
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Options {
 
     private String options;
