295159918b290cb542eb99b60dff5e5ab332d154
Matin <matin.parsapour.iam@gmail.com>
2021-11-13 15:38:10 +0330

    Create a new method to save an object of descriptive in data base

--- src/main/java/ir/maktab/University/service/DescriptiveService.java
+++ src/main/java/ir/maktab/University/service/DescriptiveService.java
@@ -4,4 +4,11 @@ import ir.maktab.University.entities.Descriptive;
 import ir.maktab.University.entities.Grade;
 
 public interface DescriptiveService extends BaseService<Descriptive,Long> {
+
+    /**
+     * Object of descriptive will save to database
+     * @param descriptive object of descriptive includes : title, header(problem), correct answer
+     * @param grade point of the question
+     */
+    void createNewDescriptive(Descriptive descriptive, double grade);
 }
--- src/main/java/ir/maktab/University/service/impl/DescriptiveServiceImpl.java
+++ src/main/java/ir/maktab/University/service/impl/DescriptiveServiceImpl.java
@@ -3,8 +3,11 @@ package ir.maktab.University.service.impl;
 import ir.maktab.University.entities.Descriptive;
 import ir.maktab.University.repository.DescriptiveRepository;
 import ir.maktab.University.service.DescriptiveService;
+import ir.maktab.University.service.QuestionHeaderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class DescriptiveServiceImpl extends BaseServiceImpl<Descriptive,Long, DescriptiveRepository>
@@ -12,9 +15,18 @@ public class DescriptiveServiceImpl extends BaseServiceImpl<Descriptive,Long, De
 
     private final DescriptiveRepository descriptiveRepository;
 
+    private final QuestionHeaderService questionHeaderService;
+
     @Autowired
-    public DescriptiveServiceImpl(DescriptiveRepository descriptiveRepository) {
+    public DescriptiveServiceImpl(DescriptiveRepository descriptiveRepository, QuestionHeaderService questionHeaderService) {
         super(descriptiveRepository);
         this.descriptiveRepository = descriptiveRepository;
+        this.questionHeaderService = questionHeaderService;
+    }
+
+    @Override
+    public void createNewDescriptive(Descriptive descriptive, double grade) {
+        Descriptive savedDescriptive = save(descriptive);
+        questionHeaderService.createNewQuestionHeaderByDescriptive(savedDescriptive,grade);
     }
 }
