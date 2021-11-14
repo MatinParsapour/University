bde1fbd5c6c37c1d042b1530fa30d284cf2c3a30
Matin <matin.parsapour.iam@gmail.com>
2021-11-14 17:16:39 +0330

    Add a method to find a course by quiz

--- src/main/java/ir/maktab/University/repository/CourseRepository.java
+++ src/main/java/ir/maktab/University/repository/CourseRepository.java
@@ -1,6 +1,7 @@
 package ir.maktab.University.repository;
 
 import ir.maktab.University.entities.Course;
+import ir.maktab.University.entities.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,4 +11,6 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
     Course findByCourseCode(long courseCode);
 
     List<Course> findAllByIsActiveTrue();
+
+    Course findByQuizzes(Quiz quiz);
 }
