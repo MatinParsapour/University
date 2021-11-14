1f5a5e0c0ecc94f597e7810b284ad5c8e0fde950
Matin <matin.parsapour.iam@gmail.com>
2021-11-14 17:28:29 +0330

    Add a method to find question by question header

--- src/main/java/ir/maktab/University/repository/QuestionRepository.java
+++ src/main/java/ir/maktab/University/repository/QuestionRepository.java
@@ -1,7 +1,10 @@
 package ir.maktab.University.repository;
 
 import ir.maktab.University.entities.Question;
+import ir.maktab.University.entities.QuestionHeader;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question,Long> {
+
+    Question findByQuestionHeader(QuestionHeader questionHeader);
 }
