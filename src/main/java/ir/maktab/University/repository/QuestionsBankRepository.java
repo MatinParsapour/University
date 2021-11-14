a29f3ec59020a4fcee59c8373c0b4b5472812781
Matin <matin.parsapour.iam@gmail.com>
2021-11-14 17:31:53 +0330

    Add a method to find all questions bank by course

--- src/main/java/ir/maktab/University/repository/QuestionsBankRepository.java
+++ src/main/java/ir/maktab/University/repository/QuestionsBankRepository.java
@@ -1,7 +1,12 @@
 package ir.maktab.University.repository;
 
+import ir.maktab.University.entities.Course;
 import ir.maktab.University.entities.QuestionsBank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionsBankRepository extends JpaRepository<QuestionsBank,Long> {
+
+    QuestionsBank findAllByCourses(Course course);
 }
