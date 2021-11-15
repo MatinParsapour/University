873a3680da2949c62f114f781c0da29f430fe185
Matin <matin.parsapour.iam@gmail.com>
2021-11-15 17:52:08 +0330

    Update creating new multiple choices form to select correct answer and check when form is going to submit

--- src/main/resources/static/js/MultipleChoicesJS.js
+++ src/main/resources/static/js/MultipleChoicesJS.js
@@ -1,9 +1,21 @@
 document.getElementById("acceptButton").addEventListener("click",function(event){
     if(document.getElementById("problem").value.length === 0
-        || document.getElementById("title").value.length === 0
-        || document.getElementById("grade").value === 0){
+        || document.getElementById("title").value.length === 0){
         event.preventDefault();
         alert("لطفا تمام ورودی ها را پر کنید")
+    }
+    if(document.getElementById("grade").value <= 0){
+        event.preventDefault();
+        alert("میزان نمره باید مثبت باشد و بیشتر از صفر")
+    }
+    if(document.getElementById("selectCorrectAnswer") === null){
+        event.preventDefault();
+        alert("با زدن بر روی دکمه 'انتخاب گزینه درست' گزینه درست برای این سوال را انتخاب کنید")
+    }
+
+    if(document.getElementsByClassName("optionInput").length !== document.getElementById("correctAnswer").length){
+        event.preventDefault()
+        alert("با زدن بر روی دکمه 'انتخاب گزینه درست' لیست گزینه ها را به روز کنید و گزینه صحیح را انتخاب کنید")
     }
 })
 
@@ -12,10 +24,6 @@ $(document).ready(function () {
 
     $(".add").click(function () {
 
-
-        var total_element = $(".element").length;
-
-
         var lastid = $(".element:last").attr("id");
         var split_id = lastid.split("_");
         var nextindex = Number(split_id[1]) + 1;
@@ -23,9 +31,8 @@ $(document).ready(function () {
         $(".element:last").after("<div class='element' id='div_" + nextindex + "'></div>");
 
         $("#div_" + nextindex).append(
-            "<input th:field='${options}' type='text' name = 'option' placeholder='گزینه جدید را وارد کنید' id='txt_" + nextindex +
-            "'>&nbsp;<input type='button' id='remove_" + nextindex + "' class='remove' value='حذف گزینه'>");
-
+            "<input th:field='${options}' class='optionInput' type='text' name = 'option' placeholder='گزینه جدید را وارد کنید' id='" + nextindex +
+            "'>&nbsp;<input type='button' id='remov_" + nextindex + "' class='remove' value='حذف گزینه'>");
     });
 
 
@@ -40,3 +47,44 @@ $(document).ready(function () {
 
     });
 });
+
+function setCorrectAnswer(){
+    if(!checkInputs()){
+        alert("گزینه ها را بنویسید")
+    }else {
+        var inputs = document.getElementsByClassName("optionInput")
+        var newDiv = document.createElement('div')
+        newDiv.setAttribute("id", "selectCorrectAnswer")
+
+        var selectTag = "";
+        selectTag = "<select id='correctAnswer' name='correctAnswer'>"
+        for (let i = 0; i < inputs.length; i++) {
+            selectTag += "<option value ='" + inputs[i].value + "'>" + inputs[i].value + "</option>";
+        }
+        selectTag += "</select>"
+        newDiv.innerHTML = selectTag
+        var elementById = document.getElementById("selectCorrectAnswer");
+        if (elementById === null) {
+            document.getElementById("container").appendChild(newDiv)
+        } else {
+            document.getElementById("container").replaceChild(newDiv, elementById)
+        }
+    }
+
+}
+
+function displayTitleLength(){
+    document.getElementById("lengthOfInput").innerHTML = 40 - document.getElementById("title").value.length
+}
+
+
+function checkInputs(){
+    var inputs = document.getElementsByClassName("optionInput")
+
+    for(let i = 0; i < inputs.length; i++){
+        if (inputs[i].value.length === 0){
+            return false
+        }
+    }
+    return true
+}
