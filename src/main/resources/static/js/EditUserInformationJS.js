8d7accd0e2762e610ca5d0c5749599b3c0622b20
Matin <matin.parsapour.iam@gmail.com>
2021-11-17 01:46:51 +0330

    Update form and use ajax to send data

--- src/main/resources/static/js/EditUserInformationJS.js
+++ src/main/resources/static/js/EditUserInformationJS.js
@@ -18,3 +18,26 @@ $('form')
     .find('input:submit, button:submit')
     .prop('disabled', true)
 ;
+
+$(document).ready(function (){
+    $('#updateUserInformation').on("submit",function (event){
+        var firstName = $("#exampleFormControlInput1").val();
+        var lastName = $("#userLastName").val();
+        var birthDay = new Date($("#userBirthDay").val());
+        var email = $("#userEmail").val();
+        var nationalCode = $("#userNationalCode").val();
+        var id = $("#userId").val();
+        $.ajax({
+            method: "post",
+            url: "http://localhost:8080/update-user/update-user-information",
+            data: {firstName: firstName, lastName: lastName, birthday: birthDay, email: email, nationalCode: nationalCode, id: id},
+            success: function (){
+                alert("اطلاعات تغییر کرد")
+            },
+            error: function (){
+                alert("یه اشتباهی بوجود امده")
+            }
+        });
+        event.preventDefault()
+    })
+})
