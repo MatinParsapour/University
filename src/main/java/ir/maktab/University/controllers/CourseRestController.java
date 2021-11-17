cc063a805a9c30347815994d2f0d730c2c3b4377
Matin <matin.parsapour.iam@gmail.com>
2021-11-17 07:56:16 +0330

    Create a rest controller to get data from front and send to service for course

---
+++ src/main/java/ir/maktab/University/controllers/CourseRestController.java
@@ -0,0 +1,32 @@
+package ir.maktab.University.controllers;
+
+import ir.maktab.University.service.CourseService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("/course-rest")
+public class CourseRestController {
+
+    private final CourseService courseService;
+
+    public CourseRestController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    /**
+     * Edit course data and save it to data base
+     * @param courseId the id of course that manager made changes
+     * @param title the title that may have changed
+     * @param courseCode the course code that may have changed
+     * @param startDate the start date that may have changed
+     * @param finishDate the finish date that may have changed
+     */
+    @PostMapping("/update-course-data")
+    public void updateCourseData(long courseId, String title, long courseCode, Date startDate, Date finishDate){
+        courseService.editCourseDetails(courseId,title,courseCode,startDate,finishDate);
+    }
+}
