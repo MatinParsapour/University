a1497442e8ac95d03f7ecf0d7641756935f6e1d5
Matin <matin.parsapour.iam@gmail.com>
2021-11-17 20:21:18 +0330

    Update user repository query

--- src/main/java/ir/maktab/University/repository/UserRepository.java
+++ src/main/java/ir/maktab/University/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package ir.maktab.University.repository;
 
 import ir.maktab.University.entities.User;
+import ir.maktab.University.entities.dto.extra.NecessaryUserDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,7 +12,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUserNameAndPassword(String username, String password);
 
-    User findByUserName(String username);
+    User findByUserNameAndIsActiveTrue(String username);
 
-    List<User> findAllByFirstNameLikeOrLastNameLikeAndStatusAndTypeAndGender(String firstName, String lastName, String status, String type, String gender);
+    @Query(value = "SELECT new ir.maktab.University.entities.dto.extra.NecessaryUserDTO(" +
+            "u.firstName, u.lastName, u.birthday, u.nationalCode, u.email, " +
+            "u.type, u.gender, u.status) FROM User u WHERE u.isActive = true AND u.gender = :gender AND u.status = :status " +
+            "AND u.type = :type AND u.firstName LIKE %:name% OR u.lastName LIKE %:name%")
+    List<NecessaryUserDTO> findAllUsersByCondition(@Param("name") String name, @Param("status") String status, @Param("type") String type, @Param("gender") String gender);
 }
