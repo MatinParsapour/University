73d5b3adacbd9d7bde4d940629c49680cb15e078
Matin <matin.parsapour.iam@gmail.com>
2021-11-17 20:08:23 +0330

    Update title input of descriptive page

--- src/main/resources/static/js/DescriptiveQustionJS.js
+++ src/main/resources/static/js/DescriptiveQustionJS.js
@@ -1,9 +1,17 @@
 document.getElementById("acceptButton").addEventListener("click",function(event){
     if(document.getElementById("problem").value.length === 0
         || document.getElementById("correctAnswer").value.length === 0
-        || document.getElementById("title").value.length === 0
-        || document.getElementById("grade").value === 0){
+        || document.getElementById("title").value.length === 0){
         event.preventDefault();
         alert("لطفا تمام ورودی ها را پر کنید")
     }
+
+    if(document.getElementById("grade").value <= 0){
+        event.preventDefault();
+        alert("میزان نمره باید مثبت و بیشتر از صفر باشد")
+    }
 })
+
+function displayTitleLength(){
+    document.getElementById("lengthOfInput").innerHTML = 40 - document.getElementById("title").value.length
+}
