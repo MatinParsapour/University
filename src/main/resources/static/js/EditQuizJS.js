9d48e140ee260e72b48e110b645bdc0afc70244b
Matin <matin.parsapour.iam@gmail.com>
2021-11-19 21:42:34 +0330

    Update edit quiz template and js to work with ajax

--- src/main/resources/static/js/EditQuizJS.js
+++ src/main/resources/static/js/EditQuizJS.js
@@ -1,19 +1,19 @@
-function checkInputs(){
-    if(document.getElementById("exampleFormControlInput1").value.length === 0){
+function checkInputs() {
+    if (document.getElementById("exampleFormControlInput1").value.length === 0) {
         event.preventDefault();
         alert("ورودی را پر کنید")
     }
-    if(document.getElementById("description").value.length === 0){
+    if (document.getElementById("description").value.length === 0) {
         event.preventDefault();
         alert("ورودی را پر کنید")
     }
-    if(document.getElementById("quizTime").value.length === 0){
+    if (document.getElementById("quizTime").value.length === 0) {
         event.preventDefault();
         alert("ورودی ها را پر کنید")
     }
 }
 
-window.onload = function (){
+window.onload = function () {
     var grades = document.getElementsByClassName("points");
 
     var sum = 0;
@@ -23,65 +23,68 @@ window.onload = function (){
     document.getElementById("gradeSummation").innerHTML = sum;
 };
 
-function openQuestionDirectionPad(){
+function openQuestionDirectionPad() {
     document.getElementById("selectQuestionDirection").style.display = "block"
 }
 
-function closeQuestionDirectionPad(){
+function closeQuestionDirectionPad() {
     document.getElementById("selectQuestionDirection").style.display = "none"
 }
 
-function openQuestionTypePad(){
+function openQuestionTypePad() {
     document.getElementById("selectQuestionDirection").style.display = "none"
     document.getElementById("questionType").style.display = "block"
 }
 
-function closeQuestionTypePad(){
+function closeQuestionTypePad() {
     document.getElementById("selectQuestionDirection").style.display = "block"
     document.getElementById("questionType").style.display = "none"
 }
 
-function openQuestionsPad(){
+function openQuestionsPad() {
     document.getElementById("displayTeachers").style.display = "block"
     document.getElementById("quizData").style.display = "none"
 }
 
-function openEditPad(){
+function openEditPad() {
     document.getElementById("displayTeachers").style.display = "none"
     document.getElementById("quizData").style.display = "block"
 }
 
-function showOptions(){
-    if(!checkInputs()){
+function showOptions(div) {
+    if (!checkInputs()) {
         alert("گزینه ها را بنویسید")
-    }else {
-        var inputs = document.getElementsByClassName("optionInput")
+    } else {
+        var id = div.id;
+        var split = id.replace(/[^\d.-]/g, '');
+        var inputs = document.getElementsByClassName("optionInput backGroundBlue form-control" + split)
         var newDiv = document.createElement('div')
-        newDiv.setAttribute("id", "selectCorrectAnswer")
+        newDiv.setAttribute("id", "selectCorrectAnswer" + split)
 
         var selectTag = "";
-        selectTag = "<select id='correctAnswer' name='correctAnswer' class='backGroundBlue form-control'>"
+        selectTag = "<select id='correctAnswer' name='correctAnswer' th:class='backGroundBlue form-control'>"
         for (let i = 0; i < inputs.length; i++) {
             selectTag += "<option value ='" + inputs[i].value + "'>" + inputs[i].value + "</option>";
         }
+
         selectTag += "</select>"
         newDiv.innerHTML = selectTag
-        var elementById = document.getElementById("selectCorrectAnswer");
+        var elementById = document.getElementById("selectCorrectAnswer" + split);
         if (elementById === null) {
-            document.getElementById("multipleChoicesForm").appendChild(newDiv)
-            document.getElementById("correctAnswerLabel").style.display = "block"
+            document.getElementById("multipleChoicesForm" + split).appendChild(newDiv)
+            document.getElementById("correctAnswerLabel" + split).style.display = "block"
         } else {
-            document.getElementById("multipleChoicesForm").replaceChild(newDiv, elementById)
-            document.getElementById("correctAnswerLabel").style.display = "block"
+            document.getElementById("multipleChoicesForm" + split).replaceChild(newDiv, elementById)
+            document.getElementById("correctAnswerLabel" + split).style.display = "block"
         }
     }
 }
 
-function checkInputs(){
+function checkInputs() {
     var inputs = document.getElementsByClassName("optionInput")
 
-    for(let i = 0; i < inputs.length; i++){
-        if (inputs[i].value.length === 0){
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].value.length === 0) {
             return false
         }
     }
@@ -89,10 +92,10 @@ function checkInputs(){
 }
 
 $('#editDescriptiveForm')
-    .each(function(){
+    .each(function () {
         $(this).data('serialized', $(this).serialize())
     })
-    .on('change input', function(){
+    .on('change input', function () {
         $(this)
             .find('input:submit, button:submit')
             .prop('disabled', $(this).serialize() === $(this).data('serialized'))
@@ -102,10 +105,10 @@ $('#editDescriptiveForm')
     .prop('disabled', true)
 ;
 $('#editMultipleChoicesForm')
-    .each(function(){
+    .each(function () {
         $(this).data('serialized', $(this).serialize())
     })
-    .on('change input', function(){
+    .on('change input', function () {
         $(this)
             .find('input:submit, button:submit')
             .prop('disabled', $(this).serialize() === $(this).data('serialized'))
@@ -115,10 +118,10 @@ $('#editMultipleChoicesForm')
     .prop('disabled', true)
 ;
 $('#editQuizForm')
-    .each(function(){
+    .each(function () {
         $(this).data('serialized', $(this).serialize())
     })
-    .on('change input', function(){
+    .on('change input', function () {
         $(this)
             .find('input:submit, button:submit')
             .prop('disabled', $(this).serialize() == $(this).data('serialized'))
@@ -127,3 +130,45 @@ $('#editQuizForm')
     .find('input:submit, button:submit')
     .prop('disabled', true)
 ;
+
+$(document).ready(function (){
+    $('#descriptivePage').on("submit",function (){
+        updateEntity.abort();
+        $("#descriptivePage").submit();
+    })
+
+    $("#multipleChociesPage").on("submit",function (){
+        updateEntity.abort();
+        $("#multipleChociesPage").submit();
+    })
+
+    $("#questionsBankPage").on("submit",function (){
+        updateEntity.abort();
+        $("#questionsBankPage").submit();
+    })
+
+    $('form').on("submit",function (event){
+        var request = {
+            method: "post",
+            url: "",
+            data: $(this).serialize(),
+            success: function (){
+                alert("اطلاعات تغییر کرد")
+                location.reload();
+            },
+            error: function (){
+                alert("اشتباهی بوجود امده")
+            }
+        }
+        if (event.target.getAttribute("id") === "editDescriptiveForm"){
+            request.url = "http://localhost:8080/descriptive-rest/update-question";
+        } else if( event.target.getAttribute("id") === "editMultipleChoicesForm"){
+            request.url = "http://localhost:8080/multiple-choices-rest/update-question";
+        } else {
+            request.url = "http://localhost:8080/question-rest/update-question";
+        }
+
+        var updateEntity = $.ajax(request);
+        event.preventDefault()
+    });
+})
