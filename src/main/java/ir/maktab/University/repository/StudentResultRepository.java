6e61c667fb7ee54fa6340172adbbcc0bcf15708f
Matin <matin.parsapour.iam@gmail.com>
2021-11-24 07:39:53 +0330

    Create repository for student result

---
+++ src/main/java/ir/maktab/University/repository/StudentResultRepository.java
@@ -0,0 +1,7 @@
+package ir.maktab.University.repository;
+
+import ir.maktab.University.entities.StudentResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentResultRepository extends JpaRepository<StudentResult,Long> {
+}
