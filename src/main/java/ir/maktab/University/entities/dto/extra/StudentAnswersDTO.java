5c47a799311ee80f421920836d49dc72e40d11f2
Matin <matin.parsapour.iam@gmail.com>
2021-11-24 07:39:17 +0330

    Create a dto for all of student answers

---
+++ src/main/java/ir/maktab/University/entities/dto/extra/StudentAnswersDTO.java
@@ -0,0 +1,17 @@
+package ir.maktab.University.entities.dto.extra;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class StudentAnswersDTO {
+
+    private Long id;
+
+    private String answer;
+}
