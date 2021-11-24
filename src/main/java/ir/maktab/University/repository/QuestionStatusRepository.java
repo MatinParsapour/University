f5a217455951f3fb29407075dd3b9ea9bba01146
Matin <matin.parsapour.iam@gmail.com>
2021-11-24 07:36:17 +0330

    Add a method to find question status based on question header id

--- src/main/java/ir/maktab/University/repository/QuestionStatusRepository.java
+++ src/main/java/ir/maktab/University/repository/QuestionStatusRepository.java
@@ -1,7 +1,13 @@
 package ir.maktab.University.repository;
 
+import ir.maktab.University.entities.QuestionHeader;
 import ir.maktab.University.entities.QuestionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface QuestionStatusRepository extends JpaRepository<QuestionStatus,Long> {
+
+    @Query(value = "SELECT qs FROM QuestionStatus qs JOIN qs.questionHeader qh where qh.id = :id")
+    QuestionStatus findQuestionStatusByQuestionHeaderId(@Param("id") long id);
 }
