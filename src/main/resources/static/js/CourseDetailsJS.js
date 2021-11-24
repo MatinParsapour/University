19380b9aa77165d93c2daadca6cf6171d5a447a0
Matin <matin.parsapour.iam@gmail.com>
2021-11-24 11:05:09 +0330

    Update course details html and js to add beginning time and finish time and date of exam

--- src/main/resources/static/js/CourseDetailsJS.js
+++ src/main/resources/static/js/CourseDetailsJS.js
@@ -7,3 +7,27 @@ function closeCreateQuizPad() {
     document.getElementById("coursesPad").style.display = "none"
     document.getElementById("closeButton").style.display = "none"
 }
+
+$(document).ready(function (){
+    $("#createQuiz").on("submit",function (event){
+        var title = $("#title").val();
+        var description = $("#message").val();
+        var examDate = $("#examDate").val();
+        var fromTime = $("#from").val();
+        var toTime = $("#to").val();
+        var quizTime = $("#minutes").val();
+        var courseId = $("#courseId").val()
+        $.ajax({
+            url: "http://localhost:8080/question-rest/create-quiz",
+            type: "post",
+            data: {title: title, description: description, inDate: examDate, fromTime: fromTime, toTime: toTime, quizTime: quizTime, courseId: courseId},
+            success: function (){
+                alert("آزمون ذخیره شد")
+                location.reload();
+            },error: function (){
+                alert("مشکلی بوجود امده")
+            }
+        })
+        event.preventDefault();
+    })
+})
