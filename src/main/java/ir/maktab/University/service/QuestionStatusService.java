8b94492e8e2d44895a3cd7300105906d95c68954
Matin <matin.parsapour.iam@gmail.com>
2021-11-24 07:38:31 +0330

    Add a method to get a list of student answers and save to data base and implement it

--- src/main/java/ir/maktab/University/service/QuestionStatusService.java
+++ src/main/java/ir/maktab/University/service/QuestionStatusService.java
@@ -2,8 +2,18 @@ package ir.maktab.University.service;
 
 import ir.maktab.University.entities.QuestionHeader;
 import ir.maktab.University.entities.QuestionStatus;
+import ir.maktab.University.entities.dto.extra.StudentAnswersDTO;
+
+import java.util.List;
+import java.util.Map;
 
 public interface QuestionStatusService extends BaseService<QuestionStatus,Long> {
 
     QuestionStatus createNewQuestionStatus(QuestionHeader questionHeader, double grade);
+
+    /**
+     * Get all of student answers and save to data base
+     * @param studentAnswers all of student answers
+     */
+    void setResult(List<StudentAnswersDTO> studentAnswers);
 }
--- src/main/java/ir/maktab/University/service/impl/QuestionStatusServiceImpl.java
+++ src/main/java/ir/maktab/University/service/impl/QuestionStatusServiceImpl.java
@@ -1,24 +1,32 @@
 package ir.maktab.University.service.impl;
 
+import com.google.common.base.Splitter;
 import ir.maktab.University.entities.QuestionHeader;
 import ir.maktab.University.entities.QuestionStatus;
+import ir.maktab.University.entities.StudentResult;
+import ir.maktab.University.entities.dto.extra.StudentAnswersDTO;
 import ir.maktab.University.repository.QuestionStatusRepository;
 import ir.maktab.University.service.QuestionStatusService;
-import ir.maktab.University.service.QuestionsService;
+import ir.maktab.University.service.StudentResultService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+
 @Service
-public class QuestionStatusServiceImpl extends BaseServiceImpl<QuestionStatus,Long, QuestionStatusRepository>
+public class QuestionStatusServiceImpl extends BaseServiceImpl<QuestionStatus, Long, QuestionStatusRepository>
         implements QuestionStatusService {
 
     private final QuestionStatusRepository questionStatusRepository;
 
+    private final StudentResultService studentResultService;
+
     @Autowired
-    public QuestionStatusServiceImpl(QuestionStatusRepository questionStatusRepository) {
+    public QuestionStatusServiceImpl(QuestionStatusRepository questionStatusRepository, StudentResultService studentResultService) {
         super(questionStatusRepository);
         this.questionStatusRepository = questionStatusRepository;
-
+        this.studentResultService = studentResultService;
     }
 
     @Override
@@ -27,5 +35,15 @@ public class QuestionStatusServiceImpl extends BaseServiceImpl<QuestionStatus,Lo
         questionStatus.setQuestionHeader(questionHeader);
         questionStatus.setQuestionPoint(grade);
         return save(questionStatus);
+    }
+
+    @Override
+    public void setResult(List<StudentAnswersDTO> studentAnswers) {
+        for (StudentAnswersDTO studentAnswersDTO : studentAnswers) {
+            QuestionStatus questionStatus = questionStatusRepository.findQuestionStatusByQuestionHeaderId(studentAnswersDTO.getId());
+            StudentResult studentResult = studentResultService.addStudentResultToQuestionStatus(questionStatus, studentAnswersDTO);
+            questionStatus.getStudentResultList().add(studentResult);
+            save(questionStatus);
+        }
     }
 }
